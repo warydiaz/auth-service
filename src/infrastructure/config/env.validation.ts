9108de4057b8f0179944ededf652bac530ecef14
Angel Eduardo Diaz Mendiburu <warydiaz@hotmail.com>
2026-05-07 12:33:08 +0200

    feat: setup inicial del proyecto (RF-01)

---
+++ src/infrastructure/config/env.validation.ts
@@ -0,0 +1,13 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+
+  PORT: Joi.number().default(3000),
+
+  DATABASE_URL: Joi.string().required(),
+
+  REDIS_URL: Joi.string().required(),
+});
